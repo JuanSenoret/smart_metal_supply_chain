4ccb12a396cf1cb52e6bdce513be7594b0361f1b
SENORET Juan <juan.senoret@altran.com>
2019-02-19 10:48:38 +0100

    Added compilers settings

--- project-6/truffle-example.js
+++ project-6/truffle-example.js
@@ -33,5 +33,16 @@ module.exports = {
         gas: 4500000,
         gasPrice: 10000000000
     }
+  },
+  compilers: {
+    solc: {
+      version: "^0.5.0", // A version or constraint - Ex. "^0.5.0"
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200   // Optimize for how many times you intend to run the code
+        }
+      }
+    }
   }
 };
